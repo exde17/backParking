07999f49200b008cdbd44a589c6338e2ba0bc9c0
exde17 <mabg0630@gmail.com>
2024-07-24 16:50:55 -0500

    mert

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { Logger, ValidationPipe } from '@nestjs/common';
+// import { HttpExceptionFilter } from './utils/http-exception.filter';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const logger = new Logger('Boostrap');
+//   app.useGlobalFilters(new HttpExceptionFilter());
+
+//   app.setGlobalPrefix('api');
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//     }),
+//   );
+//   await app.listen(process.env.PORT || 3000);
+//   logger.log(`Application is running on: ${process.env.PORT || 3000}`);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
@@ -5,18 +28,21 @@ import { HttpExceptionFilter } from './utils/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Boostrap');
+  const logger = new Logger('Bootstrap');
+
   app.useGlobalFilters(new HttpExceptionFilter());
-
   app.setGlobalPrefix('api');
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT || 3000);
-  logger.log(`Application is running on: ${process.env.PORT || 3000}`);
+
+  // Asegúrate de que la aplicación escuche en 0.0.0.0
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
