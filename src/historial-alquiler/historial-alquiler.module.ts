190f859d53744351cd1be9d8dde10f4afbd03249
unknown <mabg0630@gmail.com>
2024-07-24 23:31:27 -0500

    ter

--- src/historial-alquiler/historial-alquiler.module.ts
+++ src/historial-alquiler/historial-alquiler.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HistorialAlquilerService } from './historial-alquiler.service';
 import { HistorialAlquilerController } from './historial-alquiler.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HistorialAlquiler } from './entities/historial-alquiler.entity';
+import { Alquiler } from 'src/alquiler/entities/alquiler.entity';
 
 @Module({
   controllers: [HistorialAlquilerController],
-  providers: [HistorialAlquilerService]
+  providers: [HistorialAlquilerService],
+  imports: [TypeOrmModule.forFeature([HistorialAlquiler, Alquiler])],
 })
 export class HistorialAlquilerModule {}
