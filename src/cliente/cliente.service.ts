d09a1c453fb61b0c63e8be78730fcc11e7ad264e
unknown <mabg0630@gmail.com>
2024-07-28 14:03:30 -0500

    orden

--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -45,76 +45,132 @@ export class ClienteService {
     }
   }
 
+  // async findAll() {
+  //   try {
+
+  //     // let data =[]
+  //     const clientes = await this.clienteRepository.find({
+  //       //organizo los clientes por nombre de forma ascendente
+  //       order: {
+  //         nombre: 'ASC'
+  //       }
+  //     })
+
+  //     const data = await Promise.all(clientes.map(async (item) => {
+  //       let inf = {}
+  //       let opera = 0;
+        
+  //       const debe = await this.pagoParcialRepository.findOne({
+  //         where: {
+  //           cliente: { id: item.id }
+  //         }
+  //       })
+
+  //       const sobra = await this.pagoMasRepository.findOne({
+  //         where: {
+  //           cliente: { id: item.id }
+  //         }
+  //       })
+
+  //       // console.log("sobra: ", sobra)
+        
+
+  //       if(debe){
+  //         item.novedad = true;
+
+  //         await this.clienteRepository.update(item.id, item)
+  //       }else{
+  //         item.novedad = false;
+
+  //         await this.clienteRepository.update(item.id, item)
+  //       }
+
+  //       //verifico si el cliente ya pago pero incompleto
+  //       if(debe && item.pago){ // si esta incompleto debuelvo solo lo que debe sin el valor que paga diario
+  //         opera = +debe?.valor ?? 0;
+  //       }else{  //si no deb hago lo normal
+  //         opera = ((+(item?.valor ?? 0)) + (+(debe?.valor ?? 0))) - (+(sobra?.valor ?? 0));
+  //       } 
+        
+  //       inf = {
+  //         "id": item.id,
+  //         "nombre": item.nombre,
+  //         "valor": opera,
+  //         "novedad": item.novedad,
+  //         "pago": item.pago,
+  //         "isActive": item.isActive,
+  //       };
+
+
+  //       return inf;
+
+  //     }))
+
+  //     return data;
+  //   } catch (error) {
+  //     return {
+  //       message: 'Error al obtener los clientes',
+  //       error
+  //     }
+
+  //   }
+  // }
+
   async findAll() {
     try {
-
-      // let data =[]
       const clientes = await this.clienteRepository.find({
-        //organizo los clientes por nombre de forma ascendente
         order: {
-          nombre: 'ASC'
-        }
-      })
-
+          nombre: 'ASC', // Esto asegura que los clientes se ordenen por nombre de forma ascendente
+        },
+      });
+  
       const data = await Promise.all(clientes.map(async (item) => {
-        let inf = {}
         let opera = 0;
-        
+  
         const debe = await this.pagoParcialRepository.findOne({
           where: {
-            cliente: { id: item.id }
-          }
-        })
-
+            cliente: { id: item.id },
+          },
+        });
+  
         const sobra = await this.pagoMasRepository.findOne({
           where: {
-            cliente: { id: item.id }
-          }
-        })
-
-        // console.log("sobra: ", sobra)
-        
-
-        if(debe){
-          item.novedad = true;
-
-          await this.clienteRepository.update(item.id, item)
-        }else{
-          item.novedad = false;
-
-          await this.clienteRepository.update(item.id, item)
-        }
-
-        //verifico si el cliente ya pago pero incompleto
-        if(debe && item.pago){ // si esta incompleto debuelvo solo lo que debe sin el valor que paga diario
+            cliente: { id: item.id },
+          },
+        });
+  
+        // Actualizar el estado de novedad del cliente
+        item.novedad = !!debe;
+        await this.clienteRepository.update(item.id, item);
+  
+        // Verificar si el cliente ya pagó pero incompleto
+        if (debe && item.pago) {
+          // Si está incompleto devuelvo solo lo que debe sin el valor que paga diario
           opera = +debe?.valor ?? 0;
-        }else{  //si no deb hago lo normal
+        } else {
+          // Si no debe, hago lo normal
           opera = ((+(item?.valor ?? 0)) + (+(debe?.valor ?? 0))) - (+(sobra?.valor ?? 0));
-        } 
-        
-        inf = {
-          "id": item.id,
-          "nombre": item.nombre,
-          "valor": opera,
-          "novedad": item.novedad,
-          "pago": item.pago,
-          "isActive": item.isActive,
+        }
+  
+        return {
+          id: item.id,
+          nombre: item.nombre,
+          valor: opera,
+          novedad: item.novedad,
+          pago: item.pago,
+          isActive: item.isActive,
         };
-
-
-        return inf;
-
-      }))
-
+      }));
+  
       return data;
     } catch (error) {
       return {
         message: 'Error al obtener los clientes',
-        error
-      }
-
+        error,
+      };
     }
   }
+  
 
   async findOne(id: string) {
     try {
