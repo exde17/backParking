4a2ce0f70e056dcbedc027f631cc990a3700c883
unknown <mabg0630@gmail.com>
2024-08-12 17:38:04 -0500

    tres5

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { HistorialAlquilerModule } from './historial-alquiler/historial-alquiler
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD.toString(),
+      password: `${process.env.DB_PASSWORD}`,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
